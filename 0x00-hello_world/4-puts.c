a398d10293b35f5552bc8cd300b5cce9e32d3ff5
onyedinma <onyedinmaonyekachi@yahoo.com>
2022-10-13 12:15:03 -0700

    printing a string to the stdout using the puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- *main: Main function of of type integer and returns void
- *Description:A program that prints out a string to stdout
- *Return:returns zero if successful.
- */
-
+	/**
+	 *main: Main function of of type integer and returns void
+	 *Description:A program that prints out a string to stdout
+	 *Return:returns zero if successful.
+	 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
