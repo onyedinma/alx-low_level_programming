26e4737df56340f8a7b75569aed30486043d3787
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-01-13 22:09:44 +0100

    Update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,7 +32,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) {
         current = current->next;
     }
 
-    current->prev->next = current->next;
+    if (current->prev != NULL) {
+        current->prev->next = current->next;
+    }
     if (current->next != NULL) {
         current->next->prev = current->prev;
     }
